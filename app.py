5867347cc935926410c032529d1c5c6b053ca4f1
Ben Pankow <pankow.ben@gmail.com>
2017-08-30 22:43:49 -0500

    Fix whitespace

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+	the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 	page = request.args.get("page")
 	if page == None:
 		page = "five"
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}. {page}</p>
+	return """
+	<h1>Hello heroku</h1>
+	<p>It is currently {time}. {page}</p>
 
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time, page=page)
+	<img src="http://loremflickr.com/600/400">
+	""".format(time=the_time, page=page)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+	app.run(debug=True, use_reloader=True)

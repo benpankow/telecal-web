b9412fd5de7e0933ab5e362256de980f9170ae1a
Ben Pankow <pankow.ben@gmail.com>
2017-08-30 22:42:35 -0500

    Another test

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
+	page = request.args.get("page")
+	if page == None:
+		page = "five"
     return """
     <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    <p>It is currently {time}. {page}</p>
 
     <img src="http://loremflickr.com/600/400">
-    """.format(time=request.args)
+    """.format(time=the_time, page=page)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
